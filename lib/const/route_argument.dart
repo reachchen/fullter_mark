596e9dfeb93de6fe0fe80adcddb0033f731abfc9
AndyChen <chenruiyt@163.com>
2023-09-02 10:58:59 +0800

    todoList

--- lib/const/route_argument.dart
+++ lib/const/route_argument.dart
@@ -1,6 +1,20 @@
+import 'package:flutter_application_0/model/todo.dart';
 class RegisterPageArgument {
   final String className;
   final String url;
 
   RegisterPageArgument(this.className, this.url);
 }
+
+enum OpenType {
+  Add,
+  Edit,
+  Preview,
+}
+
+class EditTodoPageArgument {
+  final OpenType openType;
+  final Todo todo;
+
+  EditTodoPageArgument({required this.openType, required this.todo});
+}      
--- lib/pages/todo_list.dart
+++ lib/pages/todo_list.dart
@@ -1,24 +1,169 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_0/const/route_argument.dart';
+import 'package:flutter_application_0/const/route_url.dart';
+import 'package:flutter_application_0/model/todo.dart';
+import 'package:flutter_application_0/model/todo_list.dart';
+import 'package:flutter_application_0/pages/edit_todo.dart';
+import 'package:flutter_application_0/utils/generate_todo.dart';
+import 'package:flutter_application_0/component/delete_todo_dialog.dart';
 
-class TodoListPage extends StatelessWidget{
+class TodoListPage extends StatefulWidget{
 
   const TodoListPage({Key? key}): super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('列表'),
-      ),
-      body: Center(
-      child: Text(
-        this.runtimeType.toString(),
-      ),
-      ),
-   );
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage>{
+
+  late TodoList todoList;
+
+
+  @override
+  void initState() {
+    super.initState();
+    todoList = TodoList(generateTodos(100));
   }
 
-  
-  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('清单'),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context,index){
+          return TodoItem(
+            todo:todoList.list[index],
+            onTap: (Todo todo){
+              Navigator.of(context).pushNamed(
+                EDIT_TODO_PAGE_URL,
+                arguments: EditTodoPageArgument(
+                  openType:OpenType.Preview,
+                  todo: todo,
+                ),
+              );
+            }, 
+            onFinished: (Todo todo){
+              setState(() {
+                todo.isFinished = !todo.isFinished;
+                todoList.update(todo);
+              });
+            },
+            onStar:(Todo todo){
+              setState(() {
+                todo.isStar = !todo.isStar;
+                todoList.update(todo);
+              });
+            },
+            onLongPress: (Todo todo) async{
+                bool result = await showCupertinoDialog(
+                  context: context, 
+                  builder: (BuildContext context){
+                    return DeleteTodoDialog(todo: todo,);
+                  });
+                  if(result){
+                    setState(() {
+                      todoList.remove(todo.id);
+                    });
+                  }
+                },
+          );
+        },
+      ),
+    );
+  }
+}
 
+
+typedef TodoEventCallback = Function(Todo todo);
+
+class TodoItem extends StatelessWidget{
+
+  final Todo todo;
+  final TodoEventCallback onStar;
+  final TodoEventCallback onFinished;
+  final TodoEventCallback onTap;
+  final TodoEventCallback onLongPress;
+
+  const TodoItem({
+    Key? key,
+    required this.todo,
+    required this.onStar,
+    required this.onFinished,
+    required this.onTap,
+    required this.onLongPress
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: todo.isFinished ? 0.3:1.0,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            left:BorderSide(
+              width:2,
+              color:todo.priority.color,
+            ),
+          ),
+        ),
+        margin: const EdgeInsets.all(10.0),
+        padding: const 
+        EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+        height:110,
+        child: GestureDetector(
+          onTap: () => onTap(todo),
+          onLongPress:() => onLongPress(todo),
+          child:Column(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: ()=> onFinished(todo),
+                        child: Image.asset(todo.isFinished ? 'assets/images/rec_selected.png'
+                        :'assets/images/rect.png',width: 25,height: 25,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: Text(todo.title),
+                        ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: ()=> onStar(todo),
+                    child: Container(
+                      child: Image.asset(todo.isStar ? 'assets/images/star.png'
+                      :'assets/images/star_normal.png',),width: 25,height: 25,
+                      ),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget> [
+                  Image.asset('assets/images/group.png',width: 25.0,height: 25.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Text(todo.timeString,),
+                    )
+                ],
+              )
+             ],
+          )
+       ),
+      ),
+    );
+  }
 }
